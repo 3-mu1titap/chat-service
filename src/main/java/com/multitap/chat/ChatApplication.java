d136c1697ea9f5a5f7282cd19e18ea36f342def0
Seong-Tae-Kim <rlatjdxo6462@gmail.com>
2024-11-26 15:53:48 +0900

    Fix: 인프라 요청 수정

--- src/main/java/com/multitap/chat/ChatApplication.java
+++ src/main/java/com/multitap/chat/ChatApplication.java
@@ -2,7 +2,6 @@ package com.multitap.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
